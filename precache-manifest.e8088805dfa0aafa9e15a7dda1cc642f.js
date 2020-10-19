34fb515e9884186a328337f2bfc8df77ab119a35
Shashi <shashitiwari1997@gmail.com>
2020-10-19 04:56:51 -0700

    Updates

---
+++ precache-manifest.e8088805dfa0aafa9e15a7dda1cc642f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d25a88335b9a1aa9481947aa5df90a",
+    "url": "/lernov/index.html"
+  },
+  {
+    "revision": "2f1de32043c2023b7976",
+    "url": "/lernov/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "cfcd0ca0b5b047fd649e",
+    "url": "/lernov/static/css/main.9dcfd584.chunk.css"
+  },
+  {
+    "revision": "2f1de32043c2023b7976",
+    "url": "/lernov/static/js/2.e8d3c9c8.chunk.js"
+  },
+  {
+    "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
+    "url": "/lernov/static/js/2.e8d3c9c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfcd0ca0b5b047fd649e",
+    "url": "/lernov/static/js/main.1e9fc5ce.chunk.js"
+  },
+  {
+    "revision": "c11af803932db243fb7f",
+    "url": "/lernov/static/js/runtime-main.f1420df7.js"
+  }
+]);
